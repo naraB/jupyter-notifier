74a5e1fcdc3eec22068a5f701b373564ed2d4318
naraB <baran.yildirim@stud.fh-rosenheim.de>
2020-04-10 22:27:33 +0200

    Implement notification methods

--- src/background.js
+++ src/background.js
@@ -1,11 +1,8 @@
-
-
-
-
 class Background {
     isJupyterNotebook;
+    sound = new Audio('./assets/sounds/zapsplat.mp3');
 
-    constructor() {}
+    constructor() { }
 
     async getValueFromStorage(key) {
         const value = new Promise((resolve, reject) => {
@@ -22,17 +19,32 @@ class Background {
 
     onMessageListener() {
         chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-            
-                if (request.event === 'play-audio') {
-                    console.log('Play Audio');
-                    return;
-                }
-                if (request.event === 'show-notification') {
-                    console.log('Show Notification');
-                    return;
-                }
+
+            if (request.event === 'play-audio') {
+                this.sound.play();
+                return;
             }
+            if (request.event === 'show-notification') {
+                this.showNotification('42h');
+                return;
+            }
+            if (request.event === 'cell-terminated') {
+                this.sound.play();
+                this.showNotification(request.runtime);
+            }
+        }
         );
+    }
+
+    showNotification(runtime) {
+        const timestamp = new Date().getTime();
+        const notificationOptions = {
+            type: 'basic',
+            title: 'Cell Terminated!',
+            message: 'Runtime: ' + runtime,
+            iconUrl: './assets/icons/notifications-black-48dp.svg'
+        }
+        chrome.notifications.create('id ' + timestamp, notificationOptions);
     }
 
     onInit() {
