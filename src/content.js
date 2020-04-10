642166d171d14cc6da1e976925c2b205c2d4c6de
naraB <baran.yildirim@stud.fh-rosenheim.de>
2020-04-10 11:23:18 +0200

    Add detection of running and terminating cells

--- src/content.js
+++ src/content.js
@@ -3,16 +3,19 @@ class Page {
    // From local storage??
    notifyCells = [];
    selectedCell;
+   runningCellQueue = [];
 
    constructor() {
       this.codeCells = this.getInitialCodeCells();
       this.selectedCell = this.getSelectedCell();
+      console.log('selected cell', this.selectedCell);
       this.injectButton();
    }
 
    initObservers() {
       this.initNewCellObserver();
       this.initSelectedCellObserver();
+      this.initRunningCellObserver();
    }
 
    initNewCellObserver() {
@@ -23,7 +26,7 @@ class Page {
                this.codeCells.push(target);
             }
          }
-      }).observe(document.body, { attributes: true, childList: true, subtree: true });
+      }).observe(document.getElementById('notebook'), { attributes: true, childList: true, subtree: true });
    }
 
    initSelectedCellObserver() {
@@ -35,7 +38,40 @@ class Page {
                this.toggleButton();
             }
          }
-      }).observe(document.getElementById('notebook'), { attributes: true, childList: true, subtree: true});
+      }).observe(document.getElementById('notebook'), { attributes: true, childList: true, subtree: true });
+   }
+
+   initRunningCellObserver() {
+      new MutationObserver((mutations) => {
+         for (let mutation of mutations) {
+            const target = mutation.target;
+            if (target.className && target.className.includes('running') && !this.runningCellQueue.some(cell => cell.element === target) && this.notifyCells.includes(target)) {
+               const terminationObserver = this.initTerminationCellObserver(target);
+               const runningCell = { element: target, startTime: new Date(), observer: terminationObserver };
+               console.log("cell is running", runningCell);
+               this.runningCellQueue.push(runningCell);
+            }
+         }
+      }).observe(document.getElementById('notebook'), { attributes: true, childList: true, subtree: true });
+   }
+
+   initTerminationCellObserver(runningCell) {
+      const observer = new MutationObserver((mutations) => {
+         for (let mutation of mutations) {
+            const target = mutation.target;
+            if (target.className && !target.className.includes('running') && this.runningCellQueue.some(cell => cell.element === target)) {
+               const terminatedCell = this.runningCellQueue.shift();
+               // Next cell runs after completion of previous
+               if (this.runningCellQueue[0]) {
+                  this.runningCellQueue[0].startTime = new Date();
+               }
+               terminatedCell.observer.disconnect();
+               console.log("cell finished running. runtime: ", this.msToTime(new Date() - terminatedCell.startTime));
+            }
+         }
+      });
+      observer.observe(runningCell, { attributes: true, childList: true, subtree: true });
+      return observer;
    }
 
 
@@ -70,6 +106,8 @@ class Page {
          } else {
             this.notifyCells.push(this.selectedCell);
          }
+         console.log('clicked', this.notifyCells);
+
          this.toggleButton();
       });
    }
@@ -79,13 +117,28 @@ class Page {
    }
 
    getSelectedCell() {
-      return document.getElementsByClassName('selected');
+      return Array.from(document.getElementsByClassName('selected'))[0];
    }
 
    getNotifyButton() {
       return '<div class="btn-group jupyter-notifier"> \
                         <button id="notify-me" class="btn btn-default notify-me" title="notify me when cells terminates">U</button> \
                      </div>';
+   }
+
+   msToTime(duration) {
+      if (!duration) {
+         return '42h';
+      }
+      let seconds = Math.floor((duration / 1000) % 60);
+      let minutes = Math.floor((duration / (1000 * 60)) % 60);
+      let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+      hours = (hours < 10) ? '0' + hours : hours;
+      minutes = (minutes < 10) ? '0' + minutes : minutes;
+      seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+      return hours + ':' + minutes + ':' + seconds;
    }
 }
 
